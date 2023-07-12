9384ba211245f7d9abe3a740cfb48b6c62ae048f
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-07-12 13:39:17 +0300

    Create models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    date_time = models.DateTimeField(auto_now_add=True)
+    mark_task = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["date_time"]
+
+    def __str__(self):
+        return f"{self.content[:20]}..."
